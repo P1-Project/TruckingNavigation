459a152f97e977a00094804d8fc4349dca4d1ccf
Krog <Rasmkrog@gmail.com>
2025-11-20 15:50:54 +0100

    Added files and connections

---
+++ src/GenBlockadeFunc.c
@@ -0,0 +1,11 @@
+//
+// Created by rasmk on 20-11-2025.
+//
+
+#include "GenBlockadeFunc.h"
+
+#include <stdio.h>
+
+void TestConGenBlockadeFunc() {
+    printf("Hello World from TestConGenBlockadeFunc\n");
+}
--- src/utils.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by rasmk on 18-11-2025.
-//
-
-#ifndef TRUCKINGNAVIGATION_UTILS_H
-#define TRUCKINGNAVIGATION_UTILS_H
-void TestUtilsConnection(void);
-#endif //TRUCKINGNAVIGATION_UTILS_H
