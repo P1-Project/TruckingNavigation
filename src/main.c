099f5acaa2cde58b4214d5759c5ae57f28634a0b
Krog <Rasmkrog@gmail.com>
2025-12-09 12:37:12 +0100

    fix: main for mapgen and Astar

--- src/main.c
+++ src/main.c
@@ -45,34 +45,25 @@ int main(void) {
 
     //runMapGen()
     runMapGen(map, MAPSIZE, restStops);
-    //int *path = 0;
-    //PrintMap(map, MAPSIZE, path);
-    //getDestination();
 
+
+
+
+
+    int pathLength = 0;
+    int resultPathLength = 0;
+    Destination destination;
     // Get start and end points
+    GetDestination(&destination,0,MAPSIZE);
+    int startIdx = CheckCoordinateSet(map, destination.startX, destination.startY, MAPSIZE);
+    int goalIdx = CheckCoordinateSet(map, destination.endX, destination.endY, MAPSIZE);
 
     // Find optimal route between start and end points
-
-    // Divide route with ~7 hour intervals (420 minutes)
-
-    // LookForNeighbor(type 3) at found points
-
-    // Find optimal route between found stops of type 3
-
-    // Divide route with ~2.5 hour intervals (150 minutes)
-
-    //Navigate to destination
-    int pathLength = 0;
-    Destination pointA;
-
-    GetDestination(&pointA,0,MAPSIZE);
-    int startIdx = CheckCoordinateSet(map, pointA.startX, pointA.startY, MAPSIZE);
-    int goalIdx = CheckCoordinateSet(map, pointA.endX, pointA.endY, MAPSIZE);
-
     int *path = runAstarPathFinding(map, MAPSIZE, startIdx, goalIdx, &pathLength);
     printf("\n");
     if (path) {
         for (int i = 0; i < pathLength; i++) {
+            // Print route
             printf("%d ", path[i]);
             //map[path[i]] = ROUTE;
         }
@@ -83,12 +74,64 @@ int main(void) {
         printf("No path found\n");
         PrintMap(map, MAPSIZE);
     }
-    free(path);
+    printf("\n\n");
+
+
+    /*
+    int *searchPointsType3 = malloc(sizeof(int)*MAPSIZE);
+    int numSearchPointsType3;
+    // Divide route with ~x hour intervals (xxx minutes)
+    DivideRoute(map, path, pathLength, searchPointsType3, &numSearchPointsType3,340);
+    // LookForNeighbor(type 3) at found points
+    int idx[numSearchPointsType3];
+    for (int i = 0; i < numSearchPointsType3; i++) {
+        idx[i] = LookForNeighbor(map, searchPointsType3[i], MAPSIZE, TYPE3STOP,5);
+        printf("index : %d\n",idx[i]);
+        if (idx[i] == -1) {
+            printf("Failure");
+            exit(-1);
+        }
+    }
+    int *newPath = malloc(sizeof(int) * MAPSIZE);
+    int newPathLength = 0;
+    int *resultPath = malloc(sizeof(int) * MAPSIZE * MAPSIZE);
+    newPath = runAstarPathFinding(map, MAPSIZE, startIdx, idx[0], &newPathLength);
+    for (int j = 0; j < newPathLength; j++) {
+        *resultPath = newPath[j];
+        resultPath++;
+    }
+    resultPathLength +=newPathLength;
+
+    //print resultpath
+    for (int i = 0; i < resultPathLength; i++) {
+        printf("%d ", resultPath[i]);
+    }
+    */
+
+
+
+    // Find optimal route between found stops of type 3
+
+
+
+    // Divide route with ~2.5 hour intervals (150 minutes)
+
+    //Navigate to destination
+
     // LookForNeighbor(type 2) at found points
 
     // Find optimal route between found stops of type 2, 3 and start and end points
 
-    // Print route
 
+
+
+
+
+    /*
+    free(newPath);
+    free(resultPath);
+    */
+
+    free(path);
     return 0;
 }
