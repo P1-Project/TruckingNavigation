57fe5c27c5f09749941a0e913e564aa28468387d
Krog <Rasmkrog@gmail.com>
2025-12-16 11:42:18 +0100

    fix: minor spacing removed

--- src/main.c
+++ src/main.c
@@ -9,19 +9,16 @@ int main(void) {
     printf("Hello, World!\n");
     int map[MAPSIZE*MAPSIZE];
     Stops restStops[NUMBEROFSTOPS];
-    srand(time(NULL)); // NOLINT(*-msc51-cpp)
+    srand(time(NULL));
     // Find optimal route between start and end points
     Destination destination;
     GetDestination(&destination,0,MAPSIZE);
     //runMapGen()
     RunMapGen(map, MAPSIZE, restStops);
-    int pathLength = 0;
-    int numSections = 0;
+    int pathLength = 0, numSections = 0;
     int *stops = malloc(sizeof(int) * MAPSIZE * MAPSIZE);
     int *path = Navigate(map, MAPSIZE, destination, &pathLength, &numSections, stops);
     //call NavigateWrapper for printing of map with stops
     NavigateWrapper(map , MAPSIZE, path, pathLength, stops, numSections);
-    free(stops);
-    free(path);
-    return 0; //End program
+    free(stops); free(path);
 }
