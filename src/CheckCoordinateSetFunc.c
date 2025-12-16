a05d07641dcfd68ea91e049f983c6808ae56d436
Krog <Rasmkrog@gmail.com>
2025-12-16 10:36:04 +0100

    fix: minor spacing removed

--- src/CheckCoordinateSetFunc.c
+++ src/CheckCoordinateSetFunc.c
@@ -1,21 +1,12 @@
-//
-// Created by rasmk on 20-11-2025.
-//
-
 #include "CheckCoordinateSetFunc.h"
 #include "DefineConst.h"
 #include "HelperFunc.h"
-
 #include <stdio.h>
 #include <stdlib.h>
-
-
 
 void TestConCheckCoordinateSetFunc() {
     printf("Hello, World from CheckCoordinateSetFunc!\n");
 }
-
-
 /**
  * Description: Validating a given destination (x,y) - changing to nearest index if index is a blockade
  * @param map
@@ -100,146 +91,3 @@ int CheckCoordinateSet(int *map, int x, int y, int mapSize){
 
     return NOSPACEAVAILABLE;                       // Return error if no space available
 }
-
-/*
-
-int runTestIdx63() {
-    int Map[] = {5,0,5,5,5,0,0,5,
-                 0,0,5,5,5,0,0,0,
-                 0,0,0,0,0,0,0,0,
-                 5,0,0,5,5,0,0,0,
-                 5,0,0,5,5,5,0,0,
-                 0,0,0,5,5,5,0,0,
-                 0,0,0,0,0,0,0,5,
-                 5,0,0,0,0,0,5,5};
-
-    int mapSize = 8;
-    return CheckCoordinateSet(Map, 7, 7, mapSize);
-}
-
-int runTestIdx0() {
-    int Map[] = {5,0,5,5,5,0,0,5,
-                 0,0,5,5,5,0,0,0,
-                 0,0,0,0,0,0,0,0,
-                 5,0,0,5,5,0,0,0,
-                 5,0,0,5,5,5,0,0,
-                 0,0,0,5,5,5,0,0,
-                 0,0,0,0,0,0,0,5,
-                 5,0,0,0,0,0,5,5};
-
-    int mapSize = 8;
-    return CheckCoordinateSet(Map, 0, 0, mapSize);
-}
-
-int runTestIdx3() {
-    int Map[] = {5,0,5,5,5,0,0,5,
-                 0,0,5,5,5,0,0,0,
-                 0,0,0,0,0,0,0,0,
-                 5,0,0,5,5,0,0,0,
-                 5,0,0,5,5,5,0,0,
-                 0,0,0,5,5,5,0,0,
-                 0,0,0,0,0,0,0,5,
-                 5,0,0,0,0,0,5,5};
-
-    int mapSize = 8;
-    return CheckCoordinateSet(Map, 3, 0, mapSize);
-}
-
-int runTestIdx7() {
-    int Map[] = {5,0,5,5,5,0,0,5,
-                 0,0,5,5,5,0,0,0,
-                 0,0,0,0,0,0,0,0,
-                 5,0,0,5,5,0,0,0,
-                 5,0,0,5,5,5,0,0,
-                 0,0,0,5,5,5,0,0,
-                 0,0,0,0,0,0,0,5,
-                 5,0,0,0,0,0,5,5};
-
-    int mapSize = 8;
-    int res = 6;
-    return CheckCoordinateSet(Map, 7, 0, mapSize);
-}
-
-int runTestIdx56() {
-    int Map[] = {5,0,5,5,5,0,0,5,
-                 0,0,5,5,5,0,0,0,
-                 0,0,0,0,0,0,0,0,
-                 5,0,0,5,5,0,0,0,
-                 5,0,0,5,5,5,0,0,
-                 0,0,0,5,5,5,0,0,
-                 0,0,0,0,0,0,0,5,
-                 5,0,0,0,0,0,5,5};
-
-    int mapSize = 8;
-    return CheckCoordinateSet(Map, 0, 7, mapSize);
-}
-
-int runTestIdx24() {
-    int Map[] = {5,0,5,5,5,0,0,5,
-                 0,0,5,5,5,0,0,0,
-                 0,0,0,0,0,0,0,0,
-                 5,0,0,5,5,0,0,0,
-                 5,0,0,5,5,5,0,0,
-                 0,0,0,5,5,5,0,0,
-                 0,0,0,0,0,0,0,5,
-                 5,0,0,0,0,0,5,5};
-
-    int mapSize = 8;
-    return CheckCoordinateSet(Map, 0, 3, mapSize);
-}
-
-int runTestIdx36() {
-    int Map[] = {5,0,5,5,5,0,0,5,
-                 0,0,5,5,5,0,0,0,
-                 0,0,0,0,0,0,0,0,
-                 5,0,0,5,5,0,0,0,
-                 5,0,0,5,5,5,0,0,
-                 0,0,0,5,5,5,0,0,
-                 0,0,0,0,0,0,0,5,
-                 5,0,0,0,0,0,5,5};
-
-    int mapSize = 8;
-    return CheckCoordinateSet(Map, 4, 4, mapSize);
-}
-
-int runTestIdx70() {
-    int Map[] = {5,0,5,5,5,0,0,5,
-                 0,0,5,5,5,0,0,0,
-                 0,0,0,0,0,0,0,0,
-                 5,0,0,5,5,0,0,0,
-                 5,0,0,5,5,5,0,0,
-                 0,0,0,5,5,5,0,0,
-                 0,0,0,0,0,0,0,5,
-                 5,0,0,0,0,0,5,5};
-
-    int mapSize = 8;
-    return CheckCoordinateSet(Map, 6, 8, mapSize);
-}
-
-int runTestIdxNULL() {
-    int Map[] = {5,5,5,5,5,5,5,5,
-                 5,5,5,5,5,5,5,5,
-                 5,5,5,5,5,5,5,5,
-                 5,5,5,5,5,5,5,5,
-                 5,5,5,5,5,5,5,5,
-                 5,5,5,5,5,5,5,5,
-                 5,5,5,5,5,5,5,5,
-                 5,5,5,5,5,5,5,5};
-
-    int mapSize = 8;
-    return CheckCoordinateSet(Map, 5, 5, mapSize);
-}
-
-
-void runAllTests() {
-    printf("Index63: %d\n", runTestIdx63());
-    printf("Index0: %d\n", runTestIdx0());
-    printf("Index3: %d\n", runTestIdx3());
-    printf("Index7: %d\n", runTestIdx7());
-    printf("Index56: %d\n", runTestIdx56());
-    printf("Index24: %d\n", runTestIdx24());
-    printf("Index36: %d\n", runTestIdx36());
-    printf("Index70: %d\n", runTestIdx70());
-    printf("IndexNULL: %d\n", runTestIdxNULL());
-    }
-*/
--- src/main.c
+++ src/main.c
@@ -1,43 +1,26 @@
 #include <stdio.h>
-
-#include "HelperFunc.h"
 #include "MapGenFunc.h"
 #include "GetDestinationFunc.h"
 #include "DefineConst.h"
 #include "DefineStruct.h"
 #include "Navigation.h"
 
-
 int main(void) {
     printf("Hello, World!\n");
-    //connection testing
-    /*
-    TestConGenStopsFunc(); TestConGetDestinationFunc(); TestConCheckCoordinateSetFunc(); TestConGenBlockadeFunc();
-    TestMapGenConnection(); TestAstarPathFindingConnection();
-    */
     int map[MAPSIZE*MAPSIZE];
     Stops restStops[NUMBEROFSTOPS];
     srand(time(NULL)); // NOLINT(*-msc51-cpp)
     // Find optimal route between start and end points
     Destination destination;
     GetDestination(&destination,0,MAPSIZE);
-
     //runMapGen()
     RunMapGen(map, MAPSIZE, restStops);
-
-    /*int originalPathLength = 0;
-    int *originalPath = OriginalPath(map, MAPSIZE, destination, &originalPathLength);
-    free(originalPath);*/
-
     int pathLength = 0;
     int numSections = 0;
     int *stops = malloc(sizeof(int) * MAPSIZE * MAPSIZE);
     int *path = Navigate(map, MAPSIZE, destination, &pathLength, &numSections, stops);
-
     //call NavigateWrapper for printing of map with stops
     NavigateWrapper(map , MAPSIZE, path, pathLength, stops, numSections);
-
-
     free(stops);
     free(path);
     return 0; //End program
