6fe81701ee6f284e31e5e0fa9375f8a71a530a97
sabrina1411 <Sabrina.14.esk@gmail.com>
2025-12-17 11:11:55 +0100

    GetDestinationfunc split into two

--- src/GetDestinationFunc.c
+++ src/GetDestinationFunc.c
@@ -10,53 +10,44 @@
 #include <time.h>
 #include <stdio.h>
 
+void GetDestinationManual(Destination *destination, int mapSize) {
+    printf("Enter start X coordinate (0-%d): ", mapSize - 1);
+    scanf("%d", &destination->startX);
+    printf("Enter start Y coordinate (0-%d): ", mapSize - 1);
+    scanf("%d", &destination->startY);
+    printf("Enter end X coordinate (0-%d): ", mapSize - 1);
+    scanf("%d", &destination->endX);
+    printf("Enter end Y coordinate (0-%d): ", mapSize - 1);
+    scanf("%d", &destination->endY);
+}
 
-void GetDestination(Destination *destination, int acceptInput, int mapSize){
+void GetDestinationRandom(Destination *destination, int mapSize) {
+    // Random starting point
+    destination->startX = rand() % mapSize;
+    destination->startY = rand() % mapSize;
 
-    if (acceptInput == 1) {
+    // Generate end point on the opposite side
+    // If start is in left half, put end in right half
+    if (destination->startX < mapSize / 2) {
+        destination->endX = mapSize / 2 + rand() % (mapSize / 2);
+    } else {
+        destination->endX = rand() % (mapSize / 2);
+    }
 
-        printf("Enter start X coordinate (0-%d):", mapSize - 1);
-        scanf("%d", &destination->startX);
-
-        printf("Enter start Y coordinate (0-%d):", mapSize - 1);
-        scanf("%d", &destination->startY);
-
-        printf("Enter end X coordinate (0-%d):", mapSize - 1);
-        scanf("%d", &destination->endX);
-
-        printf("Enter end Y coordinate (0-%d):", mapSize - 1);
-        scanf("%d", &destination->endY);
-    }else { // If else == 0
-        // Random destination gen
-        destination->startX = rand() % mapSize;
-        destination->startY = rand() % mapSize;
-
-        // Generate end point on the opposite side
-
-        // If start is in left half, put end in right half
-        if (destination->startX < mapSize / 2) {
-            destination->endX = mapSize / 2 + rand() % (mapSize / 2);
-        }else {
-            destination->endX = rand() % (mapSize / 2);
-        }
-
-        // If start is in top half, put end in the bottom half
-       if (destination->startY < mapSize / 2) {
-           destination->endY = mapSize / 2 + rand() % (mapSize / 2);
-       } else {
-           destination->endY = rand() % (mapSize / 2);
-       }
+    // If start is in top half, put end in the bottom half
+    if (destination->startY < mapSize / 2) {
+        destination->endY = mapSize / 2 + rand() % (mapSize / 2);
+    } else {
+        destination->endY = rand() % (mapSize / 2);
     }
 }
 
-
-
 // What has to go in the main file
 
-void runDestination() {
+void runDestination(Destination *destination) {
     int choice;
 
-// Initialize random
+    // Initialize random
     srand(time(NULL));
 
     printf("Choose mode:\n");
@@ -64,17 +55,19 @@ void runDestination() {
     printf("Choose 0 if you want the coordinates to be generated randomly\n");
     printf("Enter choice: ");
     scanf("%d", &choice);
-    Destination destination;
-    GetDestination (&destination, choice, MAPSIZE);
+
+    if (choice == 1) {
+        GetDestinationManual(destination, MAPSIZE);
+    } else {
+        GetDestinationRandom(destination, MAPSIZE);
+    }
+
     printf("\n---Route Information---\n");
-    printf("Start Point: (%d, %d)\n", destination.startX, destination.startY);
-    printf("End Point: (%d, %d)\n", destination.endX, destination.endY);
+    printf("Start Point: (%d, %d)\n", destination->startX, destination->startY);
+    printf("End Point: (%d, %d)\n", destination->endX, destination->endY);
 
 
 
 }
 
 
-void TestConGetDestinationFunc() {
-    printf("Hello, World from GetDestinationFunc!\n");
-}
